191aceb9ab0e2b8c924614632a6b71024a78ea9c
christinae20492 <125934369+christinae20492@users.noreply.github.com>
2023-12-06 22:51:03 -0500

    class final

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const enlargeImg=()=>{
-    let selectImg=document.querySelector("img");
-    selectImg.addEventListener("click", ()=>{
-        selectImg.style.transform
-    })
+const image1={id:1,link:"pdp/hair1_2023.jpg",desc:"Hair 1<br>Oil on canvas<br>34x38<br>2023"};
+    const image2={id:2,link:"pdp/sit_down_2023.jpg",desc:"Hair 1<br>Oil on canvas<br>34x38<br>2023"};
+populateImages=()=>{
+    let imageContainer=document.getElementById('image');
+    let captionContainer=document.getElementById('caption');
+    if (image1===true) {
+        image1.addEventListener('click', ()=>{
+            window.open('view.html','popup');
+            imageContainer.innerHTML=`<img src=[this.image1.link]>`;
+            captionContainer.innerHTML=`[this.image1.desc]`;
+        })
+    }
 }
