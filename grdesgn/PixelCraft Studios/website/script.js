eb79b5e9eb10087d5b97d5f59296dd55b510382a
christinae20492 <125934369+christinae20492@users.noreply.github.com>
2024-02-23 09:31:08 -0500

    pcs first draft

--- grdesgn/PixelCraft Studios/website/script.js
+++ grdesgn/PixelCraft Studios/website/script.js
@@ -1,2 +1,16 @@
 const d = new Date();
 document.getElementById("demo").innerHTML = d.getFullYear();
+
+const f = document.getElementById('form');
+const q = document.getElementById('query');
+const google = 'https://www.google.com/search?q=site%3A+';
+const site = 'pagedart.com';
+
+function submitted(event) {
+  event.preventDefault();
+  const url = google + site + '+' + q.value;
+  const win = window.open(url, '_blank');
+  win.focus();
+}
+
+f.addEventListener('submit', submitted);
